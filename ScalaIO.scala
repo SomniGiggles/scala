988e2cb7ebd12c60d2ed1175b889fc4c0755b90c
Michael Harrison <michaelh@aptosolutions.local>
2017-12-06 16:47:30 +0000

    Added ScalaIO class files and edited output txt file name

--- ScalaIO.scala
+++ ScalaIO.scala
@@ -2,7 +2,7 @@ import java.io._
 
 object ScalaIO {
 	def main(args:Array[String]) {
-		val writer = new PrintWriter(new File("test.txt"))
+		val writer = new PrintWriter(new File("testingOutput.txt"))
 
 		writer.write("Hello Scala")
 		writer.close()
